94f996948cc8dbb0813bc8aaecbb229d237c2201
iArtemyst <iartemyst@gmail.com>
2025-11-24 11:35:10 -0800

    moved constants

--- app/components/fetch-movie-data.tsx
+++ app/components/fetch-movie-data.tsx
@@ -2,14 +2,10 @@
 
 
 import { IDailyMovieInformation } from "./movie-interfaces";
-
-const localhost = `localhost:7090`;
-const ip = `192.168.86.90:7090`;
-const newServer = `movie-rating-server-gpf9arfcesb5hahd.westus3-01.azurewebsites.net:443`;
-const hostLink = `https://` + newServer + `/MovieInfo`;
+import * as constants from "../constants";
 
 export async function FetchMovieData(): Promise<IDailyMovieInformation> {
-    const response = await fetch(hostLink, {
+    const response = await fetch(constants.hostLink, {
         method: "GET",
         headers: { Accept: "application/json" }
     });
