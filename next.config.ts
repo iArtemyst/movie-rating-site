492ab7b1398bc8db7aef1e90259dac5ddd6ef9b8
iArtemyst <iartemyst@gmail.com>
2025-11-24 13:03:19 -0800

    build 0.0.1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: process.env.NODE_ENV === "development" ? undefined : "export",
+  distDir: process.env.NODE_ENV === "development" ? undefined : "docs",
 };
 
 export default nextConfig;
