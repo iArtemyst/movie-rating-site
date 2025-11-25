f82f5d71faf9ff7e6ee0b3f31a95f2446e3b18f2
iArtemyst <iartemyst@gmail.com>
2025-11-24 18:54:54 -0800

    base path

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: process.env.NODE_ENV === "development" ? undefined : "export",
   distDir: process.env.NODE_ENV === "development" ? undefined : "docs",
+  basePath: "/movie-rating-site", /* TODO Remove */
 };
 
 export default nextConfig;
