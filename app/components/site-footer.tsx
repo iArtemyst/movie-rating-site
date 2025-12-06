2d28258e4f51bb19dc9f822b2d32f224be1efb9e
iArtemyst <iartemyst@gmail.com>
2025-12-06 14:57:57 -0800

    updating footer about div sizing

--- app/components/site-footer.tsx
+++ app/components/site-footer.tsx
@@ -19,7 +19,7 @@ function ShowAboutDiv() {
             <div className="footerAboutDiv">
                 <div className="w-fit flex flex-col place-items-center">
                 <p className="my-[.5em] font-semibold mb-[1em]">Thanks for playing my Daily Movie Rating Site!</p>
-                <div className="w-fit flex flex-col place-items-center text-[16px]">
+                <div className="w-fit flex flex-col place-items-center">
                     <div className="w-fit flex flex-row gap-[.375em]">
                     <p>Data obtained from</p>
                         <Link href="https://www.omdbapi.com/" rel="noopener noreferrer" target="_blank">
@@ -32,26 +32,20 @@ function ShowAboutDiv() {
                     </div>
                 </div>
                 </div>
-                <div className="w-fit flex flex-col place-items-center text-[16px] mb-[1em]">
-                <div className="w-fit flex flex-row gap-[.25em]">
-                    <p>Designed by</p>
-                    <Link href="https://github.com/iartemyst" rel="noopener noreferrer" target="_blank">
-                        <p className="font-bold">iArtemyst,</p>
-                    </Link>
-                    <p> with special thanks to </p>
-                    {/* <Link href="https://github.com/asundheim" rel="noopener noreferrer" target="_blank">
-                        <p className="font-bold">Ders</p>
-                    </Link> */}
-                    <p className="font-bold">Ders</p>
-                    <p>for support</p>
-                </div>
-                <div className="w-fit flex flex-row gap-[.375em]">
-                    <p>Please check out my</p>
-                    <Link href="https://eevee-feywild.com/" rel="noopener noreferrer" target="_blank">
-                        <p className="font-bold">portfolio</p>
-                    </Link>
-                    <p>for more of my work</p>
-                </div>
+                <div className="w-fit flex flex-col place-items-center mb-[1em]">
+                    <div className="w-fit flex flex-row gap-[.25em]">
+                        <p>Designed by</p>
+                        <Link href="https://github.com/iartemyst" rel="noopener noreferrer" target="_blank">
+                            <p className="font-bold">iArtemyst,</p>
+                        </Link>
+                    </div>
+                    <div className="w-fit flex flex-row gap-[.375em]">
+                        <p>Please check out my</p>
+                        <Link href="https://eevee-feywild.com/" rel="noopener noreferrer" target="_blank">
+                            <p className="font-bold">portfolio</p>
+                        </Link>
+                        <p>for more of my work</p>
+                    </div>
                 </div>
                 <p className="hideMeText">click anywhere to close</p>
             </div>
